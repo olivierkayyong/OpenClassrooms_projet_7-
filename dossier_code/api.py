9d566b3e16b00b64cd0f28e2731e9b7b1f633c2f
olivierkayyong <108343286+olivierkayyong@users.noreply.github.com>
2022-09-09 16:07:54 +0200

    Update api.py

--- dossier_code/api.py
+++ dossier_code/api.py
@@ -1,24 +1,8 @@
-# Importing library
+# Import library
 import uvicorn
-from fastapi import FastAPI
 import pandas as pd
-import numpy as np
-import pickle
-from lime.lime_tabular import LimeTabularExplainer
-from operator import itemgetter
-from sklearn.neighbors import NearestNeighbors
-from sklearn.metrics import confusion_matrix
-
-
-# Loading data & Model
-data = pd.read_csv("data_test.csv")
-y_test = pd.read_csv("y_test.csv")
-pickle_in = open("lgbm.pkl", "rb")
-classifier = pickle.load(pickle_in)
-
-# Scaled data
-data_scaled = classifier[1].transform(data)
-df_scaled = pd.DataFrame(data_scaled ,columns=data.columns, index=data.index)
+from fastapi import FastAPI
+from api_processing import *
 
 
 # Create the app object
@@ -26,44 +10,47 @@ app = FastAPI()
 
 # Index route
 @app.get('/')
-def index():
+def welcome():
     return {'message':"Hello welcome to the customer prediction"}
 
 # Customer prediction
-@app.get('/{predict}')
-def predict_creditworthiness(id_client:int):
-    """ Calculation of the customer creditworthiness . 
+@app.get('/predict')
+def customer_predict(id_client:int):
+    """ Customer creditworthiness prediction. 
     Args:
         id_client: customer identity   
     Returns:
         Prediction: the customer prediction
         Customer score: predict_proba value
     """
-    score = classifier[-1].predict_proba(np.array(df_scaled.loc[id_client]).reshape(1, -1)).flatten()
-    prediction = score[0]
-    if (prediction>0.5):
-       prediction = "The customer is solvent"
-    else:
-        prediction = "The customer is insolvent"
-        
-    return {'Prediction': prediction,
-            "Customer score":score.tolist()
-            }
-             
+    prediction = predict(id_client)      
+    return {'Prediction': prediction}
+
+# Customer predict_proba_score
+@app.get('/get-Score')
+def score(id_client:int):
+    score = predict_proba(id_client) 
+    return {'Predict_proba score': score.tolist() }
+
 # Dataframe indexes
-@app.get('/{predict}/{index}')
+@app.get('/get-Indexes')
 def indexes():
     """ Dataframe index"""
     return data.index.tolist() 
 
 # Dataframe columns
-@app.get('/{predict}/{index}/{columns}')
+@app.get('/get-Columns')
 def columns():
     """ Dataframe columns"""
     return data.columns.tolist() 
+# Dataframe columns
+@app.get('/get-Columns2')
+def columns():
+    """ Dataframe columns"""
+    return df_2.columns.tolist() 
 
 # Interpretability of the model:Local feature importance
-@app.get('/{predict}/{index}/{columns}/{local feature importance}')
+@app.get('/get-LocalFeatureImportance')
 def local_feature_importance(id_client:int):
     """ Determination of local feature importance. 
     Args:
@@ -72,24 +59,13 @@ def local_feature_importance(id_client:int):
         indices: local feature importance index
         values: lime explainer values
     """
-    lime1 = LimeTabularExplainer(data_scaled,
-                             feature_names=data.columns,
-                             class_names=["Solvable", "Non Solvable"],
-                             discretize_continuous=False)
-    exp = lime1.explain_instance(df_scaled.loc[id_client],
-                                 classifier[-1].predict_proba,
-                                 num_samples=100)
-    indices, values = [], []
-    for ind, val in sorted(exp.as_list(), key=itemgetter(1)):
-        indices.append(ind)
-        values.append(val)
-    return {'indices': indices,
-            "values": values
+    res = feature_importance_loc(id_client)
+    return {'indices': res[0],
+            "values": res[1]
             }
 
 # Similary customers
-nbrs = NearestNeighbors(n_neighbors=20, algorithm='ball_tree').fit(df_scaled)
-@app.get('/{predict}/{index}/{columns}/{local feature importance}/{customers neighbors}')
+@app.get('/get-CustomersNeighbors')
 def similary_customers(id_client:int):
     """ Determination of the similary customers with kneighbors. 
     Args:
@@ -97,32 +73,37 @@ def similary_customers(id_client:int):
     Returns:
         indices: similary customers index
     """
-    indices = nbrs.kneighbors(np.array(df_scaled.loc[id_client]).reshape(1, -1))[1].flatten()
-    return indices.tolist()
+    res = nearest_neighbors(id_client)
+    return res
 
 # Global feature importance
-@app.get('/{predict}/{index}/{columns}/{local feature importance}/{customers neighbors}/{global feature importance}')
+@app.get('/get-GlobalFeatureImportance')
 def features_globale():
     """ Determination of global feature importance.   
     Returns:
         indices: index of global feature importance
         features: list of  global feature importance
     """
-    indices = np.argsort(classifier[-1].feature_importances_)[::-1]
-    features = []
-    for i in range(20):
-        features.append(data.columns[indices[i]])
-    indexes = classifier[-1].feature_importances_[indices[range(20)]]
-    return {'indices': indexes.tolist(),
-            "features":features
+    res = features_imp_globale()
+    return {'indices': res[0].tolist(),
+            "features": res[1]
             }
 
 # Confusion matrix
-@app.get('/{predict}/{index}/{columns}/{local feature importance}/{customers neighbors}/{global feature importance}/{confusion matrix}')
+@app.get('/get-ConfusionMatrix')
 def confusion_mat():
     """ Calculation of the confusion matrix."""
-    cf_matrix = confusion_matrix(y_test, classifier.predict(data))
+    cf_matrix = conf_mat()
     return cf_matrix.tolist()
 
+# Test data
+@app.get('/get-TestData')
+def data_test():
+    """Loading test data"""
+    return test_data()
 
-# uvicorn api:app --reload
+# Test data_2
+@app.get('/get-TestData2')
+def data_test_2():
+    """Loading test data"""
+    return test_data_2()
